600cc4f0b70147a00fa6c38ab2f57a8d967e691a
Piotr Tomaszewski <pitom17@student.sdu.dk>
2021-05-20 15:56:19 +0200

    Changes fo flight

--- utilities/startVars.m
+++ utilities/startVars.m
@@ -73,8 +73,8 @@ visualizationFlightGearVars;
 
 % Simulation Settings
 takeOffDuration = 1;
-enableLanding = false;
-landingAltitude = -0.6;
+enableLanding = true;
+landingAltitude = -0.1;
 
 %% Custom Variables
 % Add your variables here:
